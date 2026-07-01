f14d51f46788c152920e6222f11dd1faf0482978
prachi_m <pm5562041@gmail.com>
2026-07-01 13:53:14 +0530

    product

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,7 +5,7 @@ const express = require('express')
 const app = express();
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
-
+const productRoute = require("./routes/productRoute");
 
 app.use(
     cors({
@@ -19,9 +19,12 @@ app.use(cookieParser())
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use("/api/product",productRoute);
 
 app.get("/", (req, res) => {
     res.send("Backend working");
 });
+
+console.log(process.env.MONGO_URI);
 
 module.exports = app;
